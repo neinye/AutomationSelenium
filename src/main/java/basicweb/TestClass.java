791a1c286017e13c54e2680fc1700b890312b094
nclima <nadia.clima@gmail.com>
2020-06-23 23:25:47 +0300

    initial commit

---
+++ src/main/java/basicweb/TestClass.java
@@ -0,0 +1,10 @@
+package basicweb;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver; //APIs or methods from selenium webdriver, used to automate web apps
+
+public class TestClass {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+    }
+}
